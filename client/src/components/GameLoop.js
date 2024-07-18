4538f0605f8645177419f5c49540cc352a5dcb54
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-07-17 21:26:43 -0700

    Functionality: Move character with WASD keys.

--- client/src/components/GameLoop.js
+++ client/src/components/GameLoop.js
@@ -5,8 +5,10 @@ import CanvasContext from './CanvasContext';
 import {MOVE_DIRECTIONS, MAP_DIMENSIONS, TILE_SIZE} from './mapConstants';
 import { MY_CHARACTER_INIT_CONFIG } from './characterConstants';
 import {checkMapCollision} from './utils';
+import {update as updateAllCharactersData} from './slices/allCharactersSlice'
 
-const GameLoop = ({children, allCharactersData}) => {
+
+const GameLoop = ({children, allCharactersData, updateAllCharactersData}) => {
     const canvasRef = useRef(null);
     const [context, setContext] = useState(null);
     useEffect(() => {
@@ -22,11 +24,33 @@ const GameLoop = ({children, allCharactersData}) => {
     const moveMyCharacter = useCallback((e) => {
         var currentPosition = mycharacterData.position;
         const key = e.key;
+        // condition passes when wasd key is pressed
         if (MOVE_DIRECTIONS[key]) {
             // ***********************************************
             // TODO: Add your move logic here
+            // Log key being pressed
+            console.log("Key pressed.")
+            // logging key pressed and movement
+            console.log("Key " + key + ": " + MOVE_DIRECTIONS[key]);
+
+            // logging current x and y coordinates when keys are pressed
+            console.log("X: " + currentPosition["x"] + ", Y: " + currentPosition["y"]);
+            
+            // Extract movement from an array (ex. [0, 1]) into horizontal and vertical movement (dx, dy)
+            const [dx, dy] = MOVE_DIRECTIONS[key];
+
+            // create a new character with the same character config, except updated position
+            const newCharacter = {
+                ...mycharacterData,
+                position: {
+                    x: currentPosition.x + dx,
+                    y: currentPosition.y + dy,
+                },
+            };
+            // Update the character data for just your character, while retaining all users
+            updateAllCharactersData({...allCharactersData, [MY_CHARACTER_INIT_CONFIG.id]: newCharacter});
         }
-    }, [mycharacterData]);
+    }, [mycharacterData, updateAllCharactersData]);
 
     const tick = useCallback(() => {
         if (context != null) {
@@ -62,8 +86,13 @@ const GameLoop = ({children, allCharactersData}) => {
     );
 };
 
+
 const mapStateToProps = (state) => {
     return {allCharactersData: state.allCharacters.users};
 };
 
-export default connect(mapStateToProps, {})(GameLoop);
+const mapDispatch = {updateAllCharactersData};
+
+
+export default connect(mapStateToProps, mapDispatch)(GameLoop);
+
