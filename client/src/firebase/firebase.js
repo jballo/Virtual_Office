fc4f5212b662cd9a57971be53ba567da20fc9715
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-07-17 21:15:36 -0700

    Base project. Note: .env files added to gitignore. Hardcoded keys removed.

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
+    databaseURL: "https://oa-virtoffice-project-default-rtdb.firebaseio.com/",
+  };
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseDatabase = getDatabase(firebaseApp);
